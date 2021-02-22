73954c0485e1676bfbe296defe91b96d36e75152
Chedlyrdissi <chedlyrd2000@gmail.com>
2021-02-22 18:22:01 -0500

    created migraitons

---
+++ db/migrations/20210222181200-create-athletes.sql
@@ -0,0 +1,7 @@
+CREATE TABLE athletes (
+	id int,
+	name varchar(300),
+	dob date,
+	identified_gender varchar(50),
+	PRIMARY KEY (id)
+);
---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE athletes (
+	id int,
+	name varchar(300),
+	dob date,
+	identified_gender varchar(50),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE schema_migrations (
+ 	migration varchar(255),
+ 	migrated_at time,
+ 	PRIMARY KEY (migration)
+);
+
+
